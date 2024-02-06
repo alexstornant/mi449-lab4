b7fe011651a034c3e4753cd5f6249e5bfa10925d
alexstornant <stornant.alex@gmail.com>
2024-02-06 16:20:22 -0500

    submitting lab 4

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,19 @@
 // The export statement makes these settings available to other files in 11ty
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
+
+  eleventyConfig.addShortcode("planet-card", function (color1, color2, name, details, id="none") {
+    return `<div class="card-container" id="${id}">
+    <div class="circle" style="
+    width: 130px;
+    height: 130px;
+    border-radius: 50%;
+    background: radial-gradient(circle, ${color1}, ${color2});
+    transition: transform .2s;">
+    </div>
+    <p class="card-name">${name}</p>
+    <p class="card-details">${details}</p>
+</div>`
+  });
 };
